05002b1c74ff8ddb740c49d681d3bb4caa48ad33
Blazeneo <blazeneo1998@gamil.com>
2025-03-22 08:53:52 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: "*" },
+});
+
+app.get('/send',(req,res)=>{
+    res.sendFile(__dirname+'/sender.html');
+
+})
+app.get('/res',(req,res)=>{
+    res.sendFile(__dirname+'/reciver.html');
+
+})
+io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("offer", (data) => {
+        socket.broadcast.emit("offer", data);
+    });
+
+    socket.on("answer", (data) => {
+        socket.broadcast.emit("answer", data);
+    });
+
+    socket.on("candidate", (data) => {
+        socket.broadcast.emit("candidate", data);
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
