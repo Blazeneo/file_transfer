cef04d3564197366b89df83071320d66e1116fd4
Blazeneo <blazeneo1998@gamil.com>
2025-03-22 13:46:13 +0530

    update4

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
+const path = require('path');
 const server = http.createServer(app);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
+
 const io = new Server(server, {
     cors: { origin: "*" },
 });
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/home.html');
+    res.sendFile(path.join(__dirname+'/home.html'));
 
 })
 app.get('/send',(req,res)=>{
-    res.sendFile(__dirname+'/sender.html');
+    res.sendFile(path.join(__dirname+'/sender.html'));
 
 })
 app.get('/res',(req,res)=>{
-    res.sendFile(__dirname+'/reciver.html');
+    res.sendFile(path.join(__dirname+'/reciver.html'));
 
 })
 io.on("connection", (socket) => {
